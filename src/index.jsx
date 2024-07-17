660459ab9c80e8edafca85c720dadbf3bf079e10
serdjukow <serduykovs@yandex.ru>
2024-07-17 10:17:22 +0200

    jackpot value animation

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Analytics } from "@vercel/analytics/react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
@@ -14,7 +13,6 @@ root.render(
   <React.StrictMode>    
     <QueryClientProvider client={client}>      
         <App />
-        <Analytics/>
         <ReactQueryDevtools initialIsOpen={false} />     
     </QueryClientProvider>
   </React.StrictMode>
